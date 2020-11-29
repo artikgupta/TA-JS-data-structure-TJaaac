fd8e29d2c6e7ddb57957281bcc77fb61223deaee
artikgupta <artig9262@gmail.com>
2020-11-29 18:24:20 +0530

    completed block 3 of assignment level 3

--- block-BJaacr/code/index.js
+++ block-BJaacr/code/index.js
@@ -5,9 +5,10 @@ Only when you don't get it by yourself, comeback again and read the hint
 */
 
 // Write code for the following problems:
-console.log(got);
 
 // You have access to a variable named `got`. Which comes from `data.js` file
+var gameOfThrones = got;
+console.log(got);
 
 /*
 Hint: (Structure of got variable)
@@ -20,40 +21,120 @@ Hint: (Structure of got variable)
 
 // 1. Log the value of key "houses"
 
+console.log(gameOfThrones.houses);
+
 // 2. Log the length of the "houses" key
+
+console.log(gameOfThrones.houses.length);
 
 // 3. Log the name of all houses in got one by one
 
+for (let house of gameOfThrones.houses) {
+  console.log(house);
+}
+
 // 4. Log the name of all the people in house Starks (index 0) one by one
+
+for (let name of gameOfThrones.houses[0].people) {
+  console.log(name.name);
+}
 
 // 5. Log the name of all the people in house Lannisters (index 1) one by one
 
+for (let name of gameOfThrones.houses[1].people) {
+  console.log(name.name);
+}
+
 // 6. Log the description of all the people in house Lannisters (index 1) one by one
+
+for (let description of gameOfThrones.houses[1].people) {
+  console.log(description.description);
+}
 
 // 7. Log the name and description of all the people in house Lannisters (index 1) one by one like `I am [name] and my bio is [description]`
 
+for (let people of gameOfThrones.houses[1].people) {
+  console.log(`I am ${people.name} and my bio is ${people.description}`);
+}
+
 // 8. Log the name and description of all the people in house Starks (index 0) one by one like `I am [name] and my bio is [description]`
 
+for (let people of gameOfThrones.houses[0].people) {
+  console.log(`I am ${people.name} and my bio is ${people.description}`);
+}
+
 // 9. Log the name and description of the people in house Starks whose name is `Robb Stark`
+
+for (let people of gameOfThrones.houses[0].people) {
+  if (people.name === "Robb Stark") {
+    console.log(`I am ${people.name} and my bio is ${people.description}`);
+  }
+}
 
 // 10. Log the name and description of the people in house Lannisters whose name is `Tywin Lannister`
 
 // 11. Push the name of the houses in an array named `houseNames` and Log the array
 
+let houseNames = [];
+for (let house of gameOfThrones.houses) {
+  houseNames.push(house.name);
+}
+
 // 12. Log the size of people in Starks house
 
+console.log(gameOfThrones.houses[0].people.length);
+
 // 13. Log the size of people in Lannisters house
+
+console.log(gameOfThrones.houses[1].people.length);
 
 // 14. Add the name and size of people in an object like {Starks: 4, Lannisters: 6} and log the object
 
 // 15. Log the name of all the people of all the houses in got one by one
 
+for (let house of gameOfThrones.houses) {
+  for (let person of house.people) {
+    let { name } = person;
+    console.log(name);
+  }
+}
+
 // 16. Push all names into a new array named `allPeople` and log the value array.
+
+let allPeople = [];
+for (let people of gameOfThrones.houses) {
+  for (let name of people.people) {
+    allPeople.push(name.name);
+  }
+}
 
 // 17. Log the value and length of `allPeople` array created above
 
+console.log(allPeople);
+console.log(allPeople.length);
+
 // 18. Push all descriptions into an array named `allDescription`
+
+let allDescription = [];
+for (let people of gameOfThrones.houses) {
+  for (let description of people.people) {
+    allDescription.push(description.description);
+  }
+}
 
 // 19. Log the value and length `allDescription`
 
+console.log(allDescription);
+console.log(allDescription.length);
+
 // 20. Only the the description whose length is more than 30. (use .length property on string to get the length)
+
+var description = [];
+for (let people of gameOfThrones.houses) {
+  for (let descriptionItem of people.people) {
+    description.push(descriptionItem.description);
+  }
+  if (description.length > 30) {
+    console.log(description);
+  }
+}
